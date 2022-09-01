bae0d9411fa19ac93ebe3bdddf21972d86e12fc6
JuziYou <21656296+09baka@users.noreply.github.com>
2022-09-01 18:14:12 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,26 @@
-//Smooth rolling
-$('a').click(function () {
-    $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-    }, 300);
-    return false;
-});
-//footer
+//底部部分
 var now = new Date();
 var hour = now.getHours();
 var year = now.getFullYear();
-//Greeting
-if(hour < 3){
-    document.getElementById("hi").innerHTML = "You're still awake."
-}else if (hour < 5){
-    document.getElementById("hi").innerHTML = "Up early"
-}else if (hour < 11){
-    document.getElementById("hi").innerHTML = "Good morning"
-}else if (hour < 13){
-    document.getElementById("hi").innerHTML = "Did you eat at lunch?"
-}else if (hour < 17){
-    document.getElementById("hi").innerHTML = "Good afternoon"
-}else if (hour < 19){
-    document.getElementById("hi").innerHTML = "It's half late..."
-}else if (hour < 22){
-    document.getElementById("hi").innerHTML = "Happy laziness in progress"
-}else {
-    document.getElementById("hi").innerHTML = "Good night"
-}
-//footer year
+//打招呼
+if (hour < 3) {
+    document.getElementById("hi").innerHTML = "还不快去睡觉啦！";
+} else if (hour < 5) {
+    document.getElementById("hi").innerHTML = "你起的好早哇！";
+} else if (hour < 11) {
+    document.getElementById("hi").innerHTML = "早呀！充电完毕！";
+} else if (hour < 13) {
+    document.getElementById("hi").innerHTML = "午饭吃了啥？";
+} else if (hour < 17) {
+    document.getElementById("hi").innerHTML = "唔姆，困困的下午";
+} else if (hour < 19) {
+    document.getElementById("hi").innerHTML = "已经半晚了呢...";
+} else if (hour < 22) {
+    document.getElementById("hi").innerHTML = "愉快划水ing";
+} else {
+    document.getElementById("hi").innerHTML = "晚安安~好梦梦~";
+};
+//年
 document.getElementById("year").innerHTML = year;
+//嘤嘤嘤
+console.log("Welcome to my homepage");
